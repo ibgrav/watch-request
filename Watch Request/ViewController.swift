5925fb7ff3eb5ec77b91125502edf3e5e37c0aec
Isaac Graves <isaac@isaac-mac-mini.lan>
2019-08-13 23:52:17 -0400

    header and colors

--- Watch Request/ViewController.swift
+++ Watch Request/ViewController.swift
@@ -16,6 +16,12 @@ class LandingViewController: UIViewController, WCSessionDelegate {
     func sessionDidBecomeInactive(_ session: WCSession) { }
     func sessionDidDeactivate(_ session: WCSession) { }
     
+    @IBOutlet var sendWatchBtn: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setBtnStyle(btn:sendWatchBtn, radius:20.0, shadow:5.0)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,31 +32,26 @@ class LandingViewController: UIViewController, WCSessionDelegate {
         }
     }
     
-    @IBOutlet var sendWatchBtn: UIButton!
     @IBAction func sendWatchPress(_ sender: UIButton) {
         sendWatchBtn.isEnabled = false
-        
-        
         
         let method = UserDefaults.standard.string(forKey: "method") ?? ""
         let url = UserDefaults.standard.string(forKey: "url") ?? ""
         let body = UserDefaults.standard.string(forKey: "body") ?? ""
         let bodyType = UserDefaults.standard.string(forKey: "body") ?? ""
-        let headKey = UserDefaults.standard.string(forKey: "headKey") ?? ""
-        let headVal = UserDefaults.standard.string(forKey: "headVal") ?? ""
+        let debugMode = UserDefaults.standard.bool(forKey: "watchDebugMode")
         
         if(verifyUrl(urlString: url)) {
             
             if let validSession = session {
                 //all items to pass to watch
-                let iPhoneAppContext:[String:String] = [
+                let iPhoneAppContext:[String:Any] = [
                     "method": method,
                     "url": url,
                     "body": body,
                     "bodyType": bodyType,
-                    "headKey": headKey,
-                    "headVal": headVal
-                ]
+                    "debugMode": debugMode
+                    ]
                 
                 do {
                     //send to watch
@@ -81,6 +82,17 @@ class LandingViewController: UIViewController, WCSessionDelegate {
 
 class InfoViewController: UIViewController {
     
+    @IBOutlet var watchDebugSwitch: UISwitch!
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        UserDefaults.standard.set(watchDebugSwitch.isOn, forKey: "watchDebugMode")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let debugMode:Bool = UserDefaults.standard.bool(forKey: "watchDebugMode")
+        watchDebugSwitch.isOn = debugMode
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -96,6 +108,7 @@ class RequestViewController: UIViewController {
     @IBOutlet var httpHeaderVal: UITextField!
     @IBOutlet var httpSendBtn: UIButton!
     @IBOutlet var httpHeadersOutput: UITextView!
+    @IBOutlet var httpHeadCounter: UIStepper!
     
     override func viewWillDisappear(_ animated: Bool) {
         var bodyTypeText = "TEXT";
@@ -117,18 +130,47 @@ class RequestViewController: UIViewController {
         UserDefaults.standard.set(httpUrl.text, forKey: "url")
         UserDefaults.standard.set(httpBody.text, forKey: "body")
         UserDefaults.standard.set(bodyTypeText, forKey: "bodyType")
-        UserDefaults.standard.set(httpHeaderKey.text, forKey: "headKey")
-        UserDefaults.standard.set(httpHeaderVal.text, forKey: "headVal")
         UserDefaults.standard.set(methodText, forKey: "method")
+        UserDefaults.standard.set(headOne["val"], forKey: "headOneVal")
+        UserDefaults.standard.set(headTwo["val"], forKey: "headTwoVal")
+        UserDefaults.standard.set(headThree["val"], forKey: "headThreeVal")
+        UserDefaults.standard.set(headFour["val"], forKey: "headFourVal")
+        UserDefaults.standard.set(headOne["key"], forKey: "headOneKey")
+        UserDefaults.standard.set(headTwo["key"], forKey: "headTwoKey")
+        UserDefaults.standard.set(headThree["key"], forKey: "headThreeKey")
+        UserDefaults.standard.set(headFour["key"], forKey: "headFourKey")
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        var headerText = ""
         let url = UserDefaults.standard.string(forKey: "url") ?? ""
         let body = UserDefaults.standard.string(forKey: "body") ?? ""
         let bodyType = UserDefaults.standard.string(forKey: "body") ?? ""
         let headKey = UserDefaults.standard.string(forKey: "headKey") ?? ""
         let headVal = UserDefaults.standard.string(forKey: "headVal") ?? ""
         let method = UserDefaults.standard.string(forKey: "method") ?? ""
+        
+        headOne["key"] = UserDefaults.standard.string(forKey: "headOneKey")
+        headTwo["key"] = UserDefaults.standard.string(forKey: "headTwoKey")
+        headThree["key"] = UserDefaults.standard.string(forKey: "headThreeKey")
+        headFour["key"] = UserDefaults.standard.string(forKey: "headFourKey")
+        
+        headOne["val"] = UserDefaults.standard.string(forKey: "headOneVal")
+        headTwo["val"] = UserDefaults.standard.string(forKey: "headTwoVal")
+        headThree["val"] = UserDefaults.standard.string(forKey: "headThreeVal")
+        headFour["val"] = UserDefaults.standard.string(forKey: "headFourVal")
+        
+        headOne != [:] ? headerText += "\(headOne["key"] ?? "") : \(headOne["val"] ?? "")\n" : nil
+        headTwo != [:] ? headerText += "\(headTwo["key"] ?? "") : \(headTwo["val"] ?? "")\n" : nil
+        headThree != [:] ? headerText += "\(headThree["key"] ?? "") : \(headThree["val"] ?? "")\n" : nil
+        headFour != [:] ? headerText += "\(headFour["key"] ?? "") : \(headFour["val"] ?? "")" : nil
+        
+        headOne != [:] ? httpHeadCounter.value = 1.0 : nil
+        headTwo != [:] ? httpHeadCounter.value = 2.0 : nil
+        headThree != [:] ? httpHeadCounter.value = 3.0 : nil
+        headFour != [:] ? httpHeadCounter.value = 4.0 : nil
+        
+        httpHeadersOutput.text = headerText
         
         httpUrl.text = url
         httpBody.text = body
@@ -152,7 +194,10 @@ class RequestViewController: UIViewController {
         httpBody.layer.borderColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
         httpBody.layer.borderWidth = 0.5
         httpBody.layer.cornerRadius = 6.0
-        httpSendBtn.layer.cornerRadius = 6
+        
+        setSegmentStyle(seg:httpMethod)
+        setSegmentStyle(seg:httpBodyType)
+        setBtnStyle(btn: httpSendBtn, radius:5.0, shadow:0.0)
     }
     
     override func viewDidLoad() {
@@ -162,30 +207,62 @@ class RequestViewController: UIViewController {
     //HTTP SETTINGS ACTIONS
     
     @IBAction func headerCount(_ sender: UIStepper) {
-        if(headers.count < Int(sender.value)){
-            if(httpHeaderKey.text != "" && httpHeaderVal.text != ""){
-                headers.append(["key":httpHeaderKey.text ?? "", "val":httpHeaderVal.text ?? ""])
-                httpHeaderKey.text = ""
-                httpHeaderVal.text = ""
+        let count = Int(sender.value);
+        var headerText = ""
+        var headCheck:Bool = true;
+        
+        if(httpHeaderKey.text == "" && httpHeaderVal.text == ""){
+            headCheck = false;
+        }
+        
+        if(count == 4){
+            if(headCheck){
+                headFour["key"] = httpHeaderKey.text ?? ""
+                headFour["val"] = httpHeaderVal.text ?? ""
             } else {
                 sender.value -= 1.0;
             }
-        } else {
-            headers.remove(at: headers.count - 1)
+        } else if(count == 3){
+            headFour = [:]
+            if(headThree == [:] && headCheck) {
+                headThree["key"] = httpHeaderKey.text ?? ""
+                headThree["val"] = httpHeaderVal.text ?? ""
+            }
+        } else if(count == 2){
+            headThree = [:]
+            headFour = [:]
+            if(headTwo == [:] && headCheck) {
+                headTwo["key"] = httpHeaderKey.text ?? ""
+                headTwo["val"] = httpHeaderVal.text ?? ""
+            }
+        } else if(count == 1){
+            headTwo = [:]
+            headThree = [:]
+            headFour = [:]
+            if(headOne == [:] && headCheck) {
+                headOne["key"] = httpHeaderKey.text ?? ""
+                headOne["val"] = httpHeaderVal.text ?? ""
+            }
+        } else if(count == 0){
+            headOne = [:]
+            headTwo = [:]
+            headThree = [:]
+            headFour = [:]
         }
-        print(headers)
-        var headerText = ""
-        for header in headers {
-            headerText += "\(header["key"] ?? "") : \(header["val"] ?? "")\n"
-        }
+        
+        headOne != [:] ? headerText += "\(headOne["key"] ?? "") : \(headOne["val"] ?? "")\n" : nil
+        headTwo != [:] ? headerText += "\(headTwo["key"] ?? "") : \(headTwo["val"] ?? "")\n" : nil
+        headThree != [:] ? headerText += "\(headThree["key"] ?? "") : \(headThree["val"] ?? "")\n" : nil
+        headFour != [:] ? headerText += "\(headFour["key"] ?? "") : \(headFour["val"] ?? "")" : nil
+        
         httpHeadersOutput.text = headerText
+        httpHeaderKey.text = ""
+        httpHeaderVal.text = ""
     }
     
     @IBAction func httpSendBtnPress(_ sender: UIButton) {
         let url: String = httpUrl.text ?? ""
         let body: String = httpBody.text ?? ""
-        let headKey: String = httpHeaderKey.text ?? ""
-        let headVal: String = httpHeaderVal.text ?? ""
         var method: String = "GET"
         let methodIndex: Int = httpMethod.selectedSegmentIndex
         
@@ -205,14 +282,30 @@ class RequestViewController: UIViewController {
                     "url": url,
                     "method": method
                 ]
+                var headerVals:String = "";
                 
                 var request = URLRequest(url: URL(string: url)!)
                 request.httpMethod = method
                 
-                if(headKey != "" && headVal != "") {
-                    request.setValue(headVal, forHTTPHeaderField: headKey)
-                    requestOutput["headers"] = [headKey: headVal]
+                if(headOne["key"] != nil && headOne["val"] != nil) {
+                    request.setValue(headOne["val"], forHTTPHeaderField: headOne["key"] ?? "")
+                    headerVals += "\(headOne["key"] ?? ""): \(headOne["val"] ?? "")"
                 }
+                if(headTwo["key"] != nil && headTwo["val"] != nil) {
+                    request.setValue(headTwo["val"], forHTTPHeaderField: headTwo["key"] ?? "")
+                    headerVals += ", \(headTwo["key"] ?? ""): \(headTwo["val"] ?? "")"
+                }
+                if(headThree["key"] != nil && headThree["val"] != nil) {
+                    request.setValue(headThree["val"], forHTTPHeaderField: headThree["key"] ?? "")
+                    headerVals += ", \(headThree["key"] ?? ""): \(headThree["val"] ?? "")"
+                }
+                if(headFour["key"] != nil && headFour["val"] != nil) {
+                    request.setValue(headFour["val"], forHTTPHeaderField: headFour["key"] ?? "")
+                    headerVals += ", \(headFour["key"] ?? ""): \(headFour["val"] ?? "")"
+                }
+                
+                requestOutput["headers"] = headerVals;
+                
                 if(body != "") {
                     request.httpBody = body.data(using: String.Encoding.utf8, allowLossyConversion: false)
                     requestOutput["body"] = body
@@ -273,12 +366,15 @@ class ResponseViewController: UIViewController {
     @IBOutlet var httpShareBtn: UIButton!
     @IBOutlet var requestText: UITextView!
     @IBOutlet var responseText: UITextView!
+    @IBOutlet var responseCloseBtn: UIButton!
     
     override func viewWillAppear(_ animated: Bool) {
         requestText.text = UserDefaults.standard.string(forKey: "httpRequest") ?? ""
         responseText.text = UserDefaults.standard.string(forKey: "httpResponse") ?? ""
         
         print(UserDefaults.standard.string(forKey: "httpResponse") ?? "")
+        setBtnStyle(btn: httpShareBtn, radius: 5.0, shadow: 0.0)
+        setBtnStyle(btn: responseCloseBtn, radius: 5.0, shadow: 0.0)
     }
     
     override func viewDidLoad() {
@@ -329,4 +425,27 @@ func verifyUrl(urlString: String?) -> Bool {
     return UIApplication.shared.canOpenURL(url)
 }
 
-var headers:[[String:String]] = []
+func setBtnStyle(btn:UIButton, radius:Double, shadow:Double){
+    btn.layer.cornerRadius = CGFloat(radius)
+    btn.backgroundColor = UIColor(red: 0.0, green: 0.4, blue: 0.8, alpha: 1)
+    btn.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1).cgColor
+    btn.layer.borderWidth = 1
+    btn.layer.shadowColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).cgColor
+    btn.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+    btn.layer.shadowOpacity = 1
+    btn.layer.shadowRadius = CGFloat(shadow)
+}
+
+func setSegmentStyle(seg:UISegmentedControl){
+//    seg.layer.cornerRadius = CGFloat(5.0)
+//    seg.tintColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+//    seg.layer.shadowColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.25).cgColor
+//    seg.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+//    seg.layer.shadowOpacity = 1.0
+//    seg.layer.shadowRadius = 5.0
+}
+
+var headOne:[String:String] = [:]
+var headTwo:[String:String] = [:]
+var headThree:[String:String] = [:]
+var headFour:[String:String] = [:]
